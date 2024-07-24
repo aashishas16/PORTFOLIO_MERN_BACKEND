5e48e00be273bf563f0cb5bd498dfe0dd02aab4a
AASHISH SINGUNE <120215800+aashishas16@users.noreply.github.com>
2024-07-24 11:21:07 +0530

    err

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,9 +7,11 @@ dotenv.config()
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
   console.log(token)
+  console.log("token")
   if (!token) {
     return next(new ErrorHandler("User not Authenticated!", 400));
   }
+  console.log("authenticatd")
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
   req.user = await User.findById(decoded.id);
   next();
