c03165f4eabd09b2fe7a48011174b0183db496b3
AASHISH SINGUNE <120215800+aashishas16@users.noreply.github.com>
2024-07-24 11:39:19 +0530

    err

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,14 +4,16 @@ dotenv.config()
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
   console.log(process.env.COOKIE_EXPIRE)
+  console.log("expire")
   res
     .status(statusCode)
     .cookie("token", token, {
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
-      httpOnly: false,
-      secure:false,
+      httpOnly: true,
+    secure: true, // Ensure secure if using HTTPS
+    sameSite: 'None' // Necessary for cross-site requests
     })
     .json({
       success: true,
